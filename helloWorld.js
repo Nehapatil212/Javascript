5b22b9e09f06d63112f7e829acd77d6553d13f2d
Nehapatil212 <nehampp49@gmail.com>
2023-12-26 21:41:24 +0530

    helloworld added
    helloworld added

---
+++ helloWorld.js
@@ -0,0 +1,40 @@
+console.log("Hello JS ,I am Neha");
+
+var myName;
+console.log(myName);
+
+myName = "Neha Pramod Patil";
+console.log("Full Name : ",myName);
+
+myName = "***********************************";
+console.log(myName);
+
+myName = "I Love Only JS";
+console.log(myName);
+
+myName = "***********************************";
+console.log(myName);
+
+myName = "Neha ";
+console.log("First Name :",myName);
+
+myName = "Patil ";
+console.log("Last Name :",myName);
+
+myName = "28 ";
+console.log("Age :",myName);
+
+myName = "Jalgoan ";
+console.log("Address :",myName);
+
+myName = "425306 ";
+console.log("Pincode :",myName);
+
+myName = "Female ";
+console.log("Gender  :",myName);
+
+myName = "Unmarried ";
+console.log("Marrital status :",myName);
+
+myName = "***********************************";
+console.log(myName);
